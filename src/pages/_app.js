5ebc34ce2bcf4220127d7cbafdb9523c6a7634e9
Tiny Brick Boy <102919798+TinyBrickBoy@users.noreply.github.com>
2025-07-07 19:39:32 +0200

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
-import '../styles/globals.css'
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        {/* Tailwind CSS Browser v4 */}
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
